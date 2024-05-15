d94b1a189270719797829d745cb32cf8d2ed31ca
MD Enjamul Islam <mdenjamulislam527@gmail.com>
2024-05-15 14:09:57 +0600

    Delete API create

--- index.js
+++ index.js
@@ -43,7 +43,13 @@ async function run() {
       res.send(result);
     })
 
-    
+    // Delete a coffee from the database
+    app.delete('/coffee/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeeCollection.deleteOne(query);
+      res.send(result);
+    })
 
 
     // Send a ping to confirm a successful connection
